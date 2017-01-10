8619cf11435b5ee64c508910350809737a581cb0
xwli <buptlorraine@163.com>
2017-01-10 18:58:44 +0800

    still need to be tested

--- crossing_shortest.cpp
+++ crossing_shortest.cpp
@@ -3,8 +3,8 @@
 #include <vector>
 using namespace std;
 int num,row;
-const int maxnum=26;
-const int maxrow=26;
+const int maxnum=100;
+const int maxrow=100;
 const int maxtime=maxnum*(2+maxrow);
 bool mark[maxrow];
 int min_time;
@@ -28,7 +28,7 @@ void debug_path(int t)
 {
 	for(int i=0;i<num;i++)
 	{
-	    printf("car[%d]:",i);
+	    printf("car[%d]:from<%d,%d>to<%d,%d>\n",i,startpoint[i].x,startpoint[i].y,endpoint[i].x,endpoint[i].y);
 		for(int m=0;m<t;m++)
 		{
 			printf("(%d,%d)->",shortest_path[i][m].x,shortest_path[i][m].y);
@@ -63,11 +63,58 @@ void debug_plot(int t)
     printf("\n");
     return;
 }
+void debug_dfs(int t)
+{
+    char buffer[100][3];
+    for(int m=0;m<t+1;m++)
+    {
+        for (int i=0;i<row;i++)
+		{
+			buffer[i][0] = '>';
+			buffer[i][1] = 'X';
+			buffer[i][2] = '>';
+        }
+        for (int i=0;i<num;i++)
+		{
+            buffer[now[i][m].x][now[i][m].y] = i+'0';
+        }
+        for (int i=0;i<row;i++)
+		{
+            for (int j=0;j<3;j++)
+                printf("%c", buffer[i][j]);
+			printf("\n");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return;
+}
+void my_debug(int t) {
+    char buffer[100][3];
+    for (int i=0;i<row;i++)
+    {
+        buffer[i][0] = '>';
+        buffer[i][1] = 'X';
+        buffer[i][2] = '>';
+    }
+    for (int i=0;i<num;i++)
+    {
+        buffer[now[i][t].x][now[i][t].y] = i+'0';
+    }
+    for (int i=0;i<row;i++)
+    {
+        for (int j=0;j<3;j++)
+            printf("%c", buffer[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+    return;
+}
 bool judge_deadlock(int t)
 {
     for(int k=0;k<num;k++)
     {
-        if(now[k][t-1]!=now[k][t])
+        if(now[k][t+1]!=now[k][t])
             return false;
     }
     return true;
@@ -76,7 +123,7 @@ bool judge_end(int t)
 {
 	for(int j=0;j<num;j++)
 	{
-		if(now[j][t]!=endpoint[j])
+		if(now[j][t+1]!=endpoint[j])
             return false;
 	}
 	return true;
@@ -87,36 +134,58 @@ bool judge_exchange(int t)
 	{
 		for(int q=p+1;q<num;q++)
 		{
-			if(now[p][t+1]==now[q][t] && now[p][t]==now[q][t+1])
+			if(now[p][t+1]==now[q][t] && now[p][t]==now[q][t+1] && now[p][t]!=now[p][t+1] && now[q][t]!=now[q][t+1])
 				return true;
 		}
 	}
 	return false;
 }
+bool judge_cycle(int t)
+{
+    for(int p=0;p<num;p++)
+    {
+        if(now[p][t]!=now[p][t+1])
+        {
+            for(int q=0;q<t;q++)
+            {
+                if(now[p][t+1]==now[p][q])
+                    return true;
+            }
+        }
+    }
+    return false;
+}
 void dfs(int carnum,int time)
 {
-	if (time>min_time)
+    if (time+1>=min_time) // add equals
         return;
-	if(time>2)
+    if(carnum==num)
 	{
-		bool flag1=judge_deadlock(time);
-        if(flag1)
+//        if(time>0) // time must > 0
+//        {
+            bool flag1=judge_deadlock(time);
+            if(flag1)
+                return;
+            bool flag2=judge_cycle(time);
+            if(flag2)
+                return;
+//        }
+        bool flag3=judge_exchange(time);
+        if(flag3)
             return;
-	}
-	bool flag2=judge_exchange(time);
-	if(flag2)
-		return;
-	if(carnum==num)
-	{
-		bool flag3=judge_end(time);
-		if(flag3 && time<min_time)
-		{
-		    min_time = time;
-			for(int j=0;j<num;j++)
+		bool flag4=judge_end(time);
+		if(flag4) { // judge if time >= min_time return
+            if (time+1<min_time)
             {
-                for(int k=0;k<time;k++)
-                    shortest_path[j][k]=now[j][k];
+                min_time = time+1;
+                for(int j=0;j<num;j++)
+                {
+                    for(int k=0;k<=min_time;k++)
+                        shortest_path[j][k]=now[j][k];
+                }
             }
+            else
+                return;
 		}
 		else
 			dfs(0,time+1);
@@ -143,7 +212,7 @@ void dfs(int carnum,int time)
         mark[now[i][time+1].x]=true;
 		now[i][time+1].x=now[i][time].x;
     }
-    if(now[i][time].y==1 && now[i][time].x!=endpoint[i].x && mark[now[i][time].x-1]==false && startpoint[i].x>endpoint[i].y &&now[i][time]!=endpoint[i])  //up
+    if(now[i][time].y==1 && now[i][time].x!=endpoint[i].x && mark[now[i][time].x-1]==false && startpoint[i].x>endpoint[i].x  &&now[i][time]!=endpoint[i])  //up
     {
         mark[now[i][time].x]=false;
 		now[i][time+1].y=now[i][time].y;
@@ -154,7 +223,7 @@ void dfs(int carnum,int time)
         now[i][time+1].x=now[i][time+1].x+1;
         mark[now[i][time].x]=true;
     }
-    if(now[i][time].y==1 && now[i][time].x!=endpoint[i].x && mark[now[i][time].x+1]==false && startpoint[i].x<endpoint[i].y&&now[i][time]!=endpoint[i])
+    if(now[i][time].y==1 && now[i][time].x!=endpoint[i].x && mark[now[i][time].x+1]==false && startpoint[i].x<endpoint[i].x&&now[i][time]!=endpoint[i])
     {
         mark[now[i][time].x]=false;
 		now[i][time+1].y=now[i][time].y;
@@ -174,14 +243,16 @@ void dfs(int carnum,int time)
         now[i][time+1].y=2;
         mark[now[i][time].x]=false;
     }
-    now[i][time+1]=now[i][time];
-    dfs(carnum+1, time);
+    if (now[i][time]==endpoint[i] || now[i][time].y!=1) { // can't stop at crossing
+        now[i][time+1]=now[i][time];
+        dfs(carnum+1, time);
+    }
     return;
 }
 int main()
 {
     freopen("input.txt","r",stdin);
-    //freopen("output.txt","w",stdout);
+    freopen("output.txt","w",stdout);
     memset(mark,false,sizeof(mark));
 	while (scanf("%d %d",&row,&num)!=EOF)
 	{
